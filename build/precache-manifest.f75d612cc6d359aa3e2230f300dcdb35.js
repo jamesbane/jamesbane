aab6fa3fa29395e777516594b3ed513642d4bddf
Admin <admin@Admin.local>
2023-03-06 05:46:01 -0500

    add linkedin link

---
+++ build/precache-manifest.f75d612cc6d359aa3e2230f300dcdb35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e44f355695313cfcce551aba1eeb6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e1cee8f4f57984988fe",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "31d71c3e1e4854045e6c",
+    "url": "/static/css/main.185f4e0d.chunk.css"
+  },
+  {
+    "revision": "3e1cee8f4f57984988fe",
+    "url": "/static/js/2.35c2bf5d.chunk.js"
+  },
+  {
+    "revision": "ab4784922d7f45cc6cfd5a83588623bb",
+    "url": "/static/js/2.35c2bf5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d71c3e1e4854045e6c",
+    "url": "/static/js/main.68994520.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jamesbane/precache-manifest.eae022b54cf48a8319f8f091b27fba2f.js"
+  "/precache-manifest.f75d612cc6d359aa3e2230f300dcdb35.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jamesbane/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
